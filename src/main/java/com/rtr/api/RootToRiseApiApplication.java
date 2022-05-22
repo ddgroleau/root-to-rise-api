304e2f1ca060a63d252fc72f730d0987d57b6f21
Dan Groleau <ddgroleau@gmail.com>
2022-05-21 18:13:58 -0600

    Initial commit.

---
+++ src/main/java/com/rtr/api/RootToRiseApiApplication.java
@@ -0,0 +1,13 @@
+package com.rtr.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootToRiseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootToRiseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtr/api/RootToRiseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtr.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootToRiseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
