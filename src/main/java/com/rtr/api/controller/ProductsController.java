4b805d5340b071679c5d6038b574589cd23927fb
Dan Groleau <ddgroleau@gmail.com>
2022-05-21 18:36:37 -0600

    Updating .gitignore

--- src/main/java/com/rtr/api/controller/ProductsController.java
+++ src/main/java/com/rtr/api/controller/ProductsController.java
@@ -1,2 +1,13 @@
-package com.rtr.api.controller;public class ProductsController {
+package com.rtr.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductsController {
+
+    @GetMapping("/")
+    public String getAllProducts() {
+        return "Hello World!";
+    }
 }
