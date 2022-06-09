f6e7a0b9056c2b683c639992bfcae6e488584601
Dan Groleau <ddgroleau@gmail.com>
2022-06-08 18:51:04 -0600

    Added CORS mapping.

--- src/main/java/com/rtr/api/RootToRiseApiApplication.java
+++ src/main/java/com/rtr/api/RootToRiseApiApplication.java
@@ -4,10 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RootToRiseApiApplication {
 	private static final Logger log = LoggerFactory.getLogger(RootToRiseApiApplication.class);
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins(
+								"http://localhost:3000",
+								"http://staging.roottorisebotanicals.com",
+								"http://www.roottorisebotanicals.com"
+								);
+			}
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RootToRiseApiApplication.class, args);
 	}
